0dfc54f4dc1b2b9503e3bdcd4debee3fde6971bb
i.antonov <i.antonov@smartway.today>
2024-10-12 14:43:16 +0300

    PascalCamelCase -> camelCase

--- domain/product.go
+++ domain/product.go
@@ -3,44 +3,44 @@ package domain
 import "github.com/shopspring/decimal"
 
 type Product struct {
-	Id          int               `json:"Id"`          // Id продукта
-	Brand       Brand             `json:"Brand"`       // Бренд кроссовок
-	Factory     Factory           `json:"Factory"`     // Завод изготовитель
-	Name        string            `json:"Name"`        // Название модели кроссовка
-	Description string            `json:"Description"` // Описание модели кроссовка
-	Price       decimal.Decimal   `json:"Price"`       // Цена продукта
-	Items       []ProductItem     `json:"Items"`       // Варианты кроссовок
-	Materials   []ProductMaterial `json:"Materials"`   // Материалы изготовления
-	Images      []ProductImage    `json:"Images"`      // Картинки
+	Id          int               `json:"id"`          // Id продукта
+	Brand       Brand             `json:"brand"`       // Бренд кроссовок
+	Factory     Factory           `json:"factory"`     // Завод изготовитель
+	Name        string            `json:"name"`        // Название модели кроссовка
+	Description string            `json:"description"` // Описание модели кроссовка
+	Price       decimal.Decimal   `json:"price"`       // Цена продукта
+	Items       []ProductItem     `json:"items"`       // Варианты кроссовок
+	Materials   []ProductMaterial `json:"materials"`   // Материалы изготовления
+	Images      []ProductImage    `json:"images"`      // Картинки
 }
 
 type ProductItem struct {
-	Id         int             `json:"Id"`         // Id варианта кроссовка
-	StockCount int             `json:"StockCount"` // Кол-во на складе
-	Size       int             `json:"Size"`       // Размер
-	Weight     decimal.Decimal `json:"Weight"`     // Вес
-	Color      string          `json:"Color"`      // Цвет
+	Id         int             `json:"id"`         // Id варианта кроссовка
+	StockCount int             `json:"stockCount"` // Кол-во на складе
+	Size       int             `json:"size"`       // Размер
+	Weight     decimal.Decimal `json:"weight"`     // Вес
+	Color      string          `json:"color"`      // Цвет
 }
 
 type Brand struct {
-	Id   int    `json:"Id"`   // Id бренда кроссовок
-	Name string `json:"Name"` // Наименование бренда
+	Id   int    `json:"id"`   // Id бренда кроссовок
+	Name string `json:"name"` // Наименование бренда
 }
 
 type Factory struct {
-	Id      int    `json:"Id"`      // Id завода изготовителя
-	Name    string `json:"Name"`    // Название завода
-	Country string `json:"Country"` // Страна
-	City    string `json:"City"`    // Город
-	Address string `json:"Address"` // Конкретный адрес
+	Id      int    `json:"id"`      // Id завода изготовителя
+	Name    string `json:"name"`    // Название завода
+	Country string `json:"country"` // Страна
+	City    string `json:"city"`    // Город
+	Address string `json:"address"` // Конкретный адрес
 }
 
 type ProductImage struct {
-	Id       int    `json:"Id"`       // Id картинки
-	ImageUrl string `json:"ImageUrl"` // Ссылка на ресурс с картинкой
+	Id       int    `json:"id"`       // Id картинки
+	ImageUrl string `json:"imageUrl"` // Ссылка на ресурс с картинкой
 }
 
 type ProductMaterial struct {
-	Id   int    `json:"Id"`   // Id материала
-	Name string `json:"Name"` // Название материала
+	Id   int    `json:"id"`   // Id материала
+	Name string `json:"name"` // Название материала
 }
