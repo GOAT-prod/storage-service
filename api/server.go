5365531c24cee80466af88fee460bd5a44c49b55
i.antonov <i.antonov@smartway.today>
2024-10-12 14:35:24 +0300

    test cors

--- api/server.go
+++ api/server.go
@@ -32,10 +32,10 @@ func NewServer(ctx context.Context, logger goatlogger.Logger, cfg settings.Confi
 }
 
 func addProductHandlers(router *mux.Router, storageService service.StorageService) {
-	router.HandleFunc("/products", handlers.GetProductsHandler(storageService)).Methods(http.MethodGet)
-	router.HandleFunc("/products", handlers.AddProductHandler(storageService)).Methods(http.MethodPost)
-	router.HandleFunc("/products", handlers.UpdateProductHandler(storageService)).Methods(http.MethodPut)
-	router.HandleFunc("/product/{productId}", handlers.DeleteProductHandler(storageService)).Methods(http.MethodDelete)
+	router.HandleFunc("/products", handlers.GetProductsHandler(storageService)).Methods(http.MethodGet, http.MethodOptions)
+	router.HandleFunc("/products", handlers.AddProductHandler(storageService)).Methods(http.MethodPost, http.MethodOptions)
+	router.HandleFunc("/products", handlers.UpdateProductHandler(storageService)).Methods(http.MethodPut, http.MethodOptions)
+	router.HandleFunc("/product/{productId}", handlers.DeleteProductHandler(storageService)).Methods(http.MethodDelete, http.MethodOptions)
 }
 
 func addSwaggerHandler(router *mux.Router) {
